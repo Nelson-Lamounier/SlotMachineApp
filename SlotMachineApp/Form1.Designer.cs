f5127a19c1dcc3f4878ed7be6e87d4af79e76fd8
Nelson-Lamounier <lamounierleao@gmail.com>
2023-10-23 08:21:34 +0100

    Add project files.

---
+++ SlotMachineApp/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SlotMachineApp
+{
+    partial class SlotMachine
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SlotMachine));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.txtUserInputed = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSpin = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(224, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 152);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(99, 108);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 152);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(349, 108);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 152);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // txtUserInputed
+            // 
+            this.txtUserInputed.Location = new System.Drawing.Point(337, 284);
+            this.txtUserInputed.Name = "txtUserInputed";
+            this.txtUserInputed.Size = new System.Drawing.Size(135, 31);
+            this.txtUserInputed.TabIndex = 3;
+            this.txtUserInputed.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(136, 284);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Amount Inserted";
+            // 
+            // btnSpin
+            // 
+            this.btnSpin.Location = new System.Drawing.Point(172, 336);
+            this.btnSpin.Name = "btnSpin";
+            this.btnSpin.Size = new System.Drawing.Size(75, 35);
+            this.btnSpin.TabIndex = 5;
+            this.btnSpin.Text = "Spin";
+            this.btnSpin.UseVisualStyleBackColor = true;
+            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(303, 336);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 35);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "cake.png");
+            this.imageList1.Images.SetKeyName(1, "dolar.png");
+            this.imageList1.Images.SetKeyName(2, "fire.png");
+            // 
+            // SlotMachine
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(632, 383);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSpin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUserInputed);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "SlotMachine";
+            this.Text = "Slot Machine";
+            this.Load += new System.EventHandler(this.SlotMachine_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox txtUserInputed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSpin;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ SlotMachineApp/Form1.cs
@@ -0,0 +1,93 @@
+﻿// The application should let the user enter into a Textbox the amount of money he/she is inserting into the machine. When the user clicks the spin button, the application should display 2 randomly selected symbols. 
+// If none of the randomly displayed images match the program should inform the user that he/she has won €0. If 2 of the images match, the program should inform the user that he/she has won 2 times the amount entered.
+// If 3 images match, the system should inform the user that the user has won 3 times the amount entered. 
+// When the user clicks the exit button to exit the application the program should display the total amount of money entered into the slot machine and the total won. 
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SlotMachineApp
+{
+    public partial class SlotMachine : Form
+    {
+        public SlotMachine()
+        {
+            InitializeComponent();
+        }
+
+        // Form level variables as it will accumulative
+        decimal userTotal = 0.0m;
+        decimal totalWinner = 0.0m;
+
+        private void SlotMachine_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSpin_Click(object sender, EventArgs e)
+        {
+            decimal ammountWin = 0.0m;
+            decimal userInserted = 0.0m;
+
+            // Validate user's Input
+            if (decimal.TryParse(txtUserInputed.Text, out userInserted) && userInserted > 0)
+            {
+                // If valid input
+                Random startRan = new Random();
+                // Create randomly img index 0 to 3 to display img from the listImage box
+                int img_1 = startRan.Next(0, 3);
+                int img_2 = startRan.Next(0, 3);
+                int img_3 = startRan.Next(0, 3);
+
+                // Access the img on image list by it variable name
+                pictureBox1.Image = imageList1.Images[img_1];
+                pictureBox2.Image = imageList1.Images[img_2];
+                pictureBox3.Image = imageList1.Images[img_3];
+
+                userTotal += userInserted;
+                totalWinner += GetWinner(img_1, img_2, img_3, userInserted);
+
+
+                 
+            }
+            else
+                MessageBox.Show("Not valid");
+        }
+
+        // Method to compare the img(Index) and calculate the amount - simple be called to be private and return a decimal
+        private decimal GetWinner(int img1, int img2, int img3, decimal userInserted) // Also accept the amount inserted by the user as the comparition will be based on the user inputed
+        {
+            if (img1 == img2 && img2 == img3)
+            {
+                return userInserted * 3;
+            }
+            else if (img1 == img2 || img1 == img3 || img2 == img3)
+            {
+                return userInserted * 2;
+            }
+
+            return 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            // Exit the application and display all information for the player
+            MessageBox.Show("Total Inserted: " + userTotal.ToString("C") + "\n" + "Total Won: " + totalWinner.ToString("C"));
+
+            Application.Exit();
+        }
+    }
+}
